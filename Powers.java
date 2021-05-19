70ee7ebe16c393fc9e99f01265fcd3c795fca297
Marco Valeri <marcovaleri@hotmail.it>
2021-05-19 11:12:52 +0200

    Complete class Powers

--- Powers.java
+++ Powers.java
@@ -4,40 +4,27 @@ public class Powers {
 
     public static void main(String[] args) {
         printPowers(2);
-        // printPowers(-1);
-        // printPowers(10);
-        // System.out.println("Enter a number: ");
-        // Scanner scanner = new Scanner(System.in);
-        // int n = scanner.nextInt();
-        // printPowers(n);
-        // scanner.close();
+        printPowers(-1);
+        printPowers(10);
+        System.out.println("Enter a number: ");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        printPowers(n);
+        scanner.close();
     }
 
     public static void printPowers(int n) {
 
         System.out.println("Powes of " + n);
 
-        int result = 0;
+        int result = n * 1;
+        System.out.println(n + " to the power 1 = " + result);
 
-        System.out.print(n + " to the power 1 = " + n + "\n");
-        for (int k = 2; k <= 15; k++) {
-            System.out.print(n + " to the power " + k + " = ");
-            result+= n * n;
-            System.out.print(result + "\n");
+        for (int i = 2; i <= 15; i++) {
+            result*= n;
+            System.out.println(n + " to the power " + i + " = " + result);
+
         }
-
-        // int counter = 1;
-        // while (counter <= 15) {
-        //     int result = 0;
-            
-        //     for (int k = 1; k <= counter; k++) {
-        //         result+= n * n;
-        //         System.out.println(n + " to the power " + k + " = " + result);
-        //     }
-
-        //     counter++;
-
-        // }
         
     }
 
